52ca1fe02d4013a1409ef41461a5748abea61b69
yosukeueda33 <iwalkedalongthwalllikaeninjaa@gmail.com>
2017-03-24 11:23:55 +0900

    codes

---
+++ UWPinvokeEXEsample/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace UWPinvokeEXEsample
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void invokeButton_Click(object sender, RoutedEventArgs e)
+        {
+            sendMessage();
+        }
+
+        private async void sendMessage()
+        {
+            // Referred to the codes on the following site:
+            // https://docs.microsoft.com/en-us/windows/uwp/networking/sockets
+            try
+            {
+                //Create the StreamSocket and establish a connection to the echo server.
+                Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
+
+                //The server hostname that we will be establishing a connection to. We will be running the server and client locally,
+                //so we will use localhost as the hostname.
+                Windows.Networking.HostName serverHost = new Windows.Networking.HostName("localhost");
+
+                //Every protocol typically has a standard port number. For example HTTP is typically 80, FTP is 20 and 21, etc.
+                //For the echo server/client application we will use a random port 1337.
+                string serverPort = "50000";
+                await socket.ConnectAsync(serverHost, serverPort);
+
+                //Write data to the echo server.
+                Stream streamOut = socket.OutputStream.AsStreamForWrite();
+                StreamWriter writer = new StreamWriter(streamOut);
+                string request = "test";
+                await writer.WriteLineAsync(request);
+                await writer.FlushAsync();
+            }
+            catch (Exception e)
+            {
+                //Handle exception here.            
+            }
+        }
+    }
+}
